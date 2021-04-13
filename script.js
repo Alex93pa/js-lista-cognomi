be6eee906255b9db402230bfd154660f5ece82fc
Alex93pa <Ale.pug2@gmail.com>
2021-04-13 17:28:06 +0200

    fine

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var listaCognomi = [
 ];
 
 
-listaCognomi.push(cognomeUtente);
+// Costruisco una variabile cognomeMaiuscola che è uguale a mioUtente ma con la prima lettera maiuscola, e lo pusho in cognomi
+inizialeMaiuscola = cognomeUtente.charAt(0);
+cognomeMaiuscola = inizialeMaiuscola.toUpperCase() + cognomeUtente.slice(1);
+listaCognomi.push(cognomeMaiuscola);
+
 
 listaCognomi.sort();
 
 document.getElementById("allCognomi").innerHTML = listaCognomi.join(', ');
 
-var posizioneCognome = (listaCognomi.indexOf(cognomeUtente));
+var posizioneCognome = (listaCognomi.indexOf(cognomeMaiuscola));
 
-document.getElementById("posizioneUtente").innerHTML = ("Il tuo cognome si trova alla posionione numero " + (posizioneCognome + 1));
+document.getElementById("posizioneUtente").innerHTML = ("Il tuo cognome si trova alla posizione numero " + (posizioneCognome + 1));
